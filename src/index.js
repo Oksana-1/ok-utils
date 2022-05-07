4426f5f72aa4030d44d14d817eb96e3abafa97c2
oksana-1 <4438488@gmail.com>
2022-05-07 14:48:38 +0300

    Method addWorkingDays() added, covered with tests

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ export const addDays = (date, days) => {
 	date.setDate(date.getDate() + days);
 	return date;
 };
+export const addWorkingDays = (date, days) => {
+	if (!(date instanceof Date))
+		throw new Error("First parameter of addDays() fn should be a date!");
+	if (!Number.isInteger(days))
+		throw new Error("Second parameter of addDays() fn should be a number!");
+	for (let i = 0; i < days; i++) {
+		addDays(date, 1);
+		if (date.getDay() === 6) {
+			addDays(date, 2);
+		} else if (date.getDay() === 0) {
+			addDays(date, 1);
+		}
+	}
+	return date;
+};
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,4 @@
-import { addDays } from "../src";
+import { addDays, addWorkingDays } from "../src";
 
 describe("Tests for 'addDays' method", () => {
   test("If first argument is not instance of Date object - throws Error", () => {
@@ -30,6 +30,39 @@ describe("Tests for 'addDays' method", () => {
   test("Add a whole year correctly", () => {
     expect(addDays(new Date(1641002400000), 365)).toEqual(
       new Date(1672538400000)
+    );
+  });
+});
+describe("Tests for 'addWorkingDays' method", () => {
+  test("If first argument is not instance of Date object - throws Error", () => {
+    expect(() => {
+      addWorkingDays(123, 1);
+    }).toThrow(Error);
+    expect(() => {
+      addWorkingDays(123, 1);
+    }).toThrow("First parameter of addDays() fn should be a date!");
+  });
+  test("If second argument is not type of Number - throws Error", () => {
+    expect(() => {
+      addWorkingDays(new Date(), "test");
+    }).toThrow(Error);
+    expect(() => {
+      addWorkingDays(new Date(), "test");
+    }).toThrow("Second parameter of addDays() fn should be a number!");
+  });
+  test("If correct arguments - returns Date obj", () => {
+    expect(addWorkingDays(new Date(1645668000000), 1)).toEqual(
+      new Date(1645754400000)
+    );
+  });
+  test("If weekend - returns Date object correctly", () => {
+    expect(addWorkingDays(new Date(1645668000000), 2)).toEqual(
+      new Date(1646013600000)
+    );
+  });
+  test("Returns Date object correctly for 10 days", () => {
+    expect(addWorkingDays(new Date(1645668000000), 10)).toEqual(
+      new Date(1646877600000)
     );
   });
 });
