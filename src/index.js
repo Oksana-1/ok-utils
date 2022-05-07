f5d112a3d5f563f497b1da3b50aeecb43876ef1b
oksana-1 <4438488@gmail.com>
2022-05-07 14:46:23 +0300

    Method addDays() added, covered with tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-export const print = () => {
-	return "Hello world!";
-}
+export const addDays = (date, days) => {
+	if (!(date instanceof Date))
+		throw new Error("First parameter of addDays() fn should be a date!");
+	if (!Number.isInteger(days))
+		throw new Error("Second parameter of addDays() fn should be a number!");
+	date.setDate(date.getDate() + days);
+	return date;
+};
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,35 @@
-import { print } from "../src";
+import { addDays } from "../src";
 
-describe("Initial test", () => {
-  it("works", () => {
-    expect(print()).toBe("Hello world!");
+describe("Tests for 'addDays' method", () => {
+  test("If first argument is not instance of Date object - throws Error", () => {
+    expect(() => {
+      addDays(123, 1);
+    }).toThrow(Error);
+    expect(() => {
+      addDays(123, 1);
+    }).toThrow("First parameter of addDays() fn should be a date!");
+  });
+  test("If second argument is not type of Number - throws Error", () => {
+    expect(() => {
+      addDays(new Date(), "test");
+    }).toThrow(Error);
+    expect(() => {
+      addDays(new Date(), "test");
+    }).toThrow("Second parameter of addDays() fn should be a number!");
+  });
+  test("If correct arguments - returns Date object", () => {
+    expect(addDays(new Date(1627264800000), 1)).toEqual(
+      new Date(1627351200000)
+    );
+  });
+  test("Add to last day of month and year correctly", () => {
+    expect(addDays(new Date(1640916000000), 1)).toEqual(
+      new Date(1641002400000)
+    );
+  });
+  test("Add a whole year correctly", () => {
+    expect(addDays(new Date(1641002400000), 365)).toEqual(
+      new Date(1672538400000)
+    );
   });
 });
